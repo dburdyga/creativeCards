9df55305f018d35df3cd96db4e8cbe5c42de9971
Daria <dariaburdyga@gmail.com>
2019-09-19 14:10:35 +0300

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <slot name="app-name"></slot>
+  </footer>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+  footer {
+    margin-top: 20px;
+  }
+
+</style>
